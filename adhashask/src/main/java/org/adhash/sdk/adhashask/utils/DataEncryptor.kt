06701b67f82a14ddd5174afe28424f2994b0d3d2
AnZyuZya <nelipa.ruslan@gmail.com>
2019-09-12 23:16:14 +0300

    dataEncryptor refactor

--- adhashask/src/main/java/org/adhash/sdk/adhashask/utils/DataEncryptor.kt
+++ adhashask/src/main/java/org/adhash/sdk/adhashask/utils/DataEncryptor.kt
@@ -3,17 +3,12 @@ package org.adhash.sdk.adhashask.utils
 import java.security.MessageDigest
 
 
-class DataEncryptor  {
+class DataEncryptor {
 
-    fun checkIfAdExpected(data: String, expectedHashes: ArrayList<String>) : Boolean {
-        val hash = sha1(data)
-        return expectedHashes.contains(hash)
-    }
+    fun checkIfAdExpected(data: String, expectedHashes: ArrayList<String>) = sha1(data).run { expectedHashes.contains(this) }
 
-    private fun sha1(target: String): String {
-        val bytes = target.toByteArray()
-        val md = MessageDigest.getInstance("SHA-1")
-        val digest = md.digest(bytes)
-        return digest.fold("", { str, it -> str + "%02x".format(it) })
-    }
+    private fun sha1(target: String) = MessageDigest
+        .getInstance("SHA-1")
+        .digest(target.toByteArray())
+        .fold("", { str, it -> str + "%02x".format(it) })
 }
